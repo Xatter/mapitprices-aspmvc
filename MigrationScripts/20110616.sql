aa6c588ba9dd248a66e59457a3e3773e4b1e8267
Jim Wallace <jim@mapitprices.com>
2011-06-17 11:15:13 -0400

    new logo

--- MigrationScripts/20110616.sql
+++ MigrationScripts/20110616.sql
@@ -1,6 +1,2 @@
-﻿-- Creating table 'BetaSignups'
-CREATE TABLE [dbo].[BetaSignups] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [Email] nvarchar(max)  NOT NULL
-);
-GO
+﻿ALTER TABLE BetaInviteCodes DROP COLUMN UserID;
+ALTER TABLE BetaInviteCodes ADD User_ID int;
