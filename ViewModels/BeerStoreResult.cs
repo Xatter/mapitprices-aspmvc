7e033dbdb6ff4c5708969082d23a3b1ad3da8639
Jim Wallace <jim@jimfwallace.com>
2011-08-27 21:52:33 -0400

    Compression and a couple other sorty bits for beer app

--- ViewModels/BeerStoreResult.cs
+++ ViewModels/BeerStoreResult.cs
@@ -8,13 +8,11 @@ namespace MapItPrices.ViewModels
     public class BeerStoreResult
     {
         public int ID { get; set; }
-
         public string Name { get; set; }
-
-        public double? Latitude { get; set; }
-
-        public double? Longitude { get; set; }
-
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public double Distance { get; set; }
+
+        public object Address { get; set; }
     }
 }
