a808f2efee5d6d2645e933c6871de1ef5d47c0af
Jim Wallace <jim@jimfwallace.com>
2011-09-04 00:28:54 -0400

    oops, forgot these files

---
+++ Models/Attributes/BeerAuthenticatorAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+
+namespace MapItPrices.Models.Attributes
+{
+    public class BeerAuthorization : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            HttpRequestBase request = httpContext.Request;
+            string sessionToken = request.Headers["SessionToken"];
+
+            MapItPricesEntities db = new MapItPricesEntities();
+            User user = db.Users.SingleOrDefault(u => u.SessionToken == sessionToken);
+
+            if (user == null)
+                return false;
+
+            return true;
+        }
+    }
+}
